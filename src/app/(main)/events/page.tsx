a63bd6e9e3de3f1f8b6c1211221be35df4d72e7a
rukonbd1 <rukonbd.dev@gmail.com>
2025-04-28 11:18:22 +0600

    update navbar and create new post section

--- src/app/(main)/events/page.tsx
+++ src/app/(main)/events/page.tsx
@@ -1,7 +1,55 @@
+import { AlertIcon, EventIcon } from "@/components/icons/Icons";
+import Container from "@/components/shared/Container";
+import Image from "next/image";
+
 const EventsPage = () => {
     return (
-        <div>
-            <h1>This is event page</h1>
+        <div className="max-w-2xl mx-auto p-4 my-2">
+            <Container>
+                <section className="flex flex-row gap-2">
+                    <div>
+                        <div className="size-10 rounded-full ring">
+                            <Image src={"/avater-male.jpg"} width={40} height={40} alt="avatar" className="size-full rounded-full object-cover" />
+                        </div>
+                    </div>
+                    <span className="block cursor-pointer font-medium w-full hover:bg-gray-200 rounded border py-2 px-4">Start write here</span>
+                </section>
+                <section className="flex flex-wrap justify-end items-center gap-2 mt-4">
+                    <div>
+                        <button className="flex flex-row items-center gap-1 hover:bg-blue-300 bg-blue-200 rounded px-4 py-1 cursor-pointer">
+                            <EventIcon />
+                            <span>Event</span>
+                        </button>
+                    </div>
+                    <div>
+                        <button className="flex flex-row items-center gap-1 hover:bg-blue-300 bg-blue-200  rounded px-4 py-1 cursor-pointer">
+                            <AlertIcon />
+                            <span>Alert</span>
+                        </button>
+                    </div>
+                </section>
+            </Container>
+            <Container className="my-4 py-1 rounded">
+                <div className="flex items-center">
+                    <span className="shrink-0">Sort by</span>
+                    <span className="h-px flex-1 bg-gray-300"></span>
+                    <div className="shrink-0">
+                        <select name="sort" id="" className="outline-0">
+                            <option value="new">New</option>
+                            <option value="old">Old</option>
+                        </select>
+                    </div>
+                </div>
+            </Container>
+            <Container className="py-2">
+                <div className="flex flex-wrap gap-2 items-center">
+                    <span>Filter by:</span>
+                    <button className="rounded px-4 py-1 bg-blue-200 hover:bg-blue-300">5km</button>
+                    <button className="rounded px-4 py-1 bg-blue-200 hover:bg-blue-300">10km</button>
+                    <button className="rounded px-4 py-1 bg-blue-200 hover:bg-blue-300">city</button>
+                    <button className="rounded px-4 py-1 bg-blue-200 hover:bg-blue-300">Country</button>
+                </div>
+            </Container>
         </div>
     )
 }
--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -1,14 +1,30 @@
 'use client'
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
 
 const Menu = ({ href, label, children }: { href: string, label: string, children: React.ReactNode }) => {
+    const [activeStyle, setActiveStyle] = useState("")
+    console.log(activeStyle)
     const path = usePathname()
-    const activeStyle: string = path == href ? "bg-blue-600 text-white" : ""
+    console.log(path)
+    useEffect(() => {
+        if (path === href) {
+            setActiveStyle(' text-blue-500')
+        } else {
+            setActiveStyle('')
+        }
+    }, [path, href])
     return (
-        <Link href={href} className={"flex flex-row gap-1 items-center px-1.5 rounded " + activeStyle}>
-            {children}
-            <span>{label}</span>
+        <Link
+            href={href}
+            className={"flex flex-col items-center justify-center p-2 hover:text-blue-600 hover:bg-gray-200 rounded-full md:rounded-none md:hover:bg-transparent" + activeStyle}
+        >
+            <span className="relative">
+                <span className="bg-red-600 text-white rounded-full px-1 text-xs ml-0.5 absolute -top-1 -right-3 ">9+</span>
+                {children}
+            </span>
+            <p className="text-sm hidden md:inline-block">{label}</p>
         </Link>
     )
 }
